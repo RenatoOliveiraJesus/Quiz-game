dfce943c81b746850aa55d053fc292651689928a
RenatoOliveiraJesus <renato.oliveira.jesus100@gmail.com>
2022-05-23 17:01:35 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+<ScoreBoard :winCount="this.winCount" :loseCount="this.loseCount" />
+
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
+
+      <template v-for="(answer, index) in answers" :key="index">
+        <input
+          :disabled="this.answerSubmitted"
+          type="radio"
+          name="options"
+          :value="answer"
+          v-model="this.chosenAnswer"
+        />
+        <label v-html="answer"> </label><br />
+      </template>
+
+      <button
+        v-if="!this.answerSubmitted"
+        @click="this.submitAnswer()"
+        class="send"
+        type="button"
+      >
+        Send
+      </button>
+
+      <section class="result" v-if="this.answerSubmitted">
+        <template v-if="this.chosenAnswer == this.correctAnswer">
+          <h4>
+            &#9989; Parabéns, a resposta "{{ this.correctAnswer }}" está
+            correta.
+          </h4>
+        </template>
+        <template v-else>
+          <h4>
+            &#10060; Que pena, a resposta está errada. A resposta correta é "{{
+              this.correctAnswer
+            }}".
+          </h4>
+        </template>
+        <button @click="this.getNewQuestion()" class="send" type="button">
+          Próxima pergunta
+        </button>
+      </section>
+    </template>
+  </div>
+</template>
+
+
+<script>
+ import ScoreBoard from '@/components/ScoreBoard.vue'
+export default {
+ 
+  name: "App",
+  components: {
+    ScoreBoard
+  },
+
+  data() {
+    return {
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswer: undefined,
+      chosenAnswer: undefined,
+      answerSubmitted: false,
+      winCount: 0,
+      loseCount: 0,
+    };
+  
+  },
+  computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(
+        Math.round(Math.random() * answers.length),
+        0,
+        this.correctAnswer
+      );
+      return answers;
+    },
+  },
+  methods: {
+    submitAnswer() {
+      if (!this.chosenAnswer) {
+        alert("Selecione uma resposta");
+      } else {
+        this.answerSubmitted = true;
+        if (this.chosenAnswer == this.correctAnswer) {
+          console.log("acertou");
+            this.winCount++;
+
+        } else {
+          console.log("errou");
+            this.loseCount++;
+        }
+      }
+    },
+    getNewQuestion() {
+      this.chosenAnswer = undefined,
+      this.answerSubmitted = false;
+      this.question = undefined;
+      this.axios
+        .get("https://opentdb.com/api.php?amount=40&category=27")
+        .then((response) => {
+          this.question = response.data.results[0].question;
+          this.correctAnswer = response.data.results[0].correct_answer;
+          this.incorrectAnswers = response.data.results[0].incorrect_answers;
+        });
+        
+    },
+  },
+
+  created() {
+      this.getNewQuestion();
+  },
+};
+
+// https://opentdb.com/api.php?amount=1&category=21&difficulty=hard
+</script>
+
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  max-width: 960px;
+  margin: 60px auto;
+
+  input[type="radio"] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: white;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="score">
+    <span>Player</span>
+    <span> {{this.winCount}} </span>
+    X
+    <span> {{this.loseCount}} </span>
+    <span> Computer</span>
+  </section>
+</template>
+
+<script>
+
+    export default {
+        props: ['winCount' , 'loseCount']
+     
+    }
+
+
+
+</script>
+
+<style lang="scss">
+section.score {
+  border-bottom: 1px solid black;
+  padding: 24px;
+  font-size: 18px;
+
+  span {
+    padding: 8px;
+    font-weight: bold;
+    border: 1px solid black;
+  }
+}
+</style>
